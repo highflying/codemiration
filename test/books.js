7fced36623bb93e35d0a17c59adace78220b7345
Ben Rogers <ben@bdr.org>
2015-06-17 20:35:28 +0100

    Topic and Book file loads/parsers

---
+++ test/books.js
@@ -0,0 +1,142 @@
+var expect = require("chai").expect;
+var books  = require("../lib/books.js");
+
+var fixturePath = __dirname + "/../test-fixtures/";
+
+var bookId = "book";
+
+describe("books", function () {
+  describe("findFiles()", function () {
+    it("should find all books in a given path", function (done) {
+      expect(books).to.respondTo("findFiles");
+
+      books.findFiles(fixturePath + "books", function (err, fileList) {
+        expect(err).to.be.a("null");
+        expect(fileList).to.be.an("array");
+        expect(fileList).to.deep.equal([
+          fixturePath + "books/1invalid-json-book.json",
+          fixturePath + "books/" + bookId + ".json",
+        ]);
+
+        done();
+      });
+    });
+
+    it("should error on an invalid path", function (done) {
+      books.findFiles(
+        "made up path",
+        function (err, fileList) {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(fileList).to.be.undefined;
+
+          return done();
+        }
+      );
+    });
+  });
+
+  describe("load()", function () {
+
+    it("should read valid json file", function (done) {
+      expect(books).to.respondTo("load");
+
+      books.load(fixturePath + "books/" + bookId + ".json", function (err, bookData) {
+        expect(err).to.be.a("null");
+        expect(bookData).to.be.an("object");
+
+        expect(bookData.id).to.equal(bookId);
+        expect(bookData.title).to.be.a("string");
+        expect(bookData.subtitle).to.be.a("string");
+        expect(bookData.author).to.be.an("array");
+        expect(bookData.publisher).to.be.a("string");
+        expect(bookData.year).to.be.a("number");
+        expect(bookData.isbn).to.be.a("string");
+
+        expect(bookData.title).to.equal("Essential Skills for the Agile Developer");
+        expect(bookData.subtitle).to.equal("A Guide to Better Programming and Design");
+        expect(bookData.author).to.deep.equal(["Alan Shalloway", "Scott Bain", "Ken Pugh", "Amir Kolsky"]);
+        expect(bookData.publisher).to.equal("Addison-Wesley");
+        expect(bookData.year).to.equal(2011);
+        expect(bookData.isbn).to.equal("9780321543738");
+        
+        done();
+      });
+    });
+
+    it("should error on a missing file", function (done) {
+      books.load("made-up-filename.json", function (err, bookData) {
+        expect(bookData).to.be.a("undefined");
+        expect(err).to.be.an.instanceOf(Error);
+
+        done();
+      });
+    });
+
+    it("should error on invalid json", function (done) {
+      books.load(fixturePath + "books/1invalid-json-book.json", function (err, bookData) {
+        expect(bookData).to.be.an("undefined");
+        expect(err).to.be.an.instanceOf(Error);
+
+        done();
+      });
+    });
+  });
+
+  describe("loadAll()", function () {
+    it("should load all available books", function (done) {
+      expect(books).to.respondTo("loadAll");
+
+      books.load(
+        fixturePath + "books/" + bookId + ".json",
+        function (err, bookData) {
+          books.loadAll(
+            fixturePath + "books",
+            function (err, allBooks) {
+              expect(allBooks).to.be.an("object");
+              expect(Object.keys(allBooks)).to.have.length(1);
+              expect(allBooks).to.have.property(bookId);
+              expect(allBooks[bookId]).to.deep.equal(bookData);
+              done();
+            }
+          );
+        }
+      );
+    });
+
+    it("should error on an invalid path", function (done) {
+      books.loadAll(
+        "made up path",
+        function (err, allBooks) {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(allBooks).to.be.undefined;
+
+          return done();
+        }
+      );
+    });
+  });
+
+  describe("get()", function () {
+    it("should return a single book by id", function (done) {
+      expect(books).to.respondTo("get");
+
+      books.load(
+        fixturePath + "books/" + bookId + ".json",
+        function (err, expectedData) {
+          var bookData = books.get(bookId);
+          expect(bookData).to.be.an("object");
+          expect(bookData).to.deep.equal(expectedData);
+
+          done();
+        }
+      );
+    });
+
+    it("should handle unknown ids", function () {
+      var bookData = books.get("made-up-id");
+
+      expect(bookData).to.be.undefined;
+    });
+  });
+
+});
---
+++ test/topics.js
@@ -0,0 +1,130 @@
+var expect = require("chai").expect;
+var topics = require("../lib/topics.js");
+
+var fixturePath = __dirname + "/../test-fixtures/";
+
+describe("topics", function () {
+  describe("findFiles()", function () {
+    it("should find all topics in a given path", function (done) {
+      expect(topics).to.respondTo("findFiles");
+
+      topics.findFiles(fixturePath + "topics", function (err, fileList) {
+        expect(err).to.be.a("null");
+        expect(fileList).to.be.an("array");
+        expect(fileList).to.deep.equal([
+          fixturePath + "topics/1invalid-json-topic.json",
+          fixturePath + "topics/topic.json",
+        ]);
+
+        done();
+      });
+    });
+
+    it("should error on an invalid path", function (done) {
+      topics.findFiles(
+        "made up path",
+        function (err, fileList) {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(fileList).to.be.undefined;
+
+          return done();
+        }
+      );
+    });
+  });
+
+  describe("load()", function () {
+
+    it("should read valid json file", function (done) {
+      expect(topics).to.respondTo("load");
+
+      topics.load(fixturePath + "topics/topic.json", function (err, topicData) {
+        expect(err).to.be.a("null");
+        expect(topicData).to.be.an("object");
+
+        expect(topicData.title).to.be.a("string");
+        expect(topicData.summary).to.be.a("string");
+        expect(topicData.rationale).to.be.an("array");
+        expect(topicData.references).to.be.a("array");
+        
+        done();
+      });
+    });
+
+    it("should error on a missing file", function (done) {
+      topics.load("made-up-filename.json", function (err, topicData) {
+        expect(topicData).to.be.a("undefined");
+        expect(err).to.be.an.instanceOf(Error);
+
+        done();
+      });
+    });
+
+    it("should error on invalid json", function (done) {
+      topics.load(fixturePath + "topics/1invalid-json-topic.json", function (err, topicData) {
+        expect(topicData).to.be.an("undefined");
+        expect(err).to.be.an.instanceOf(Error);
+
+        done();
+      });
+    });
+  });
+
+  describe("loadAll()", function () {
+    it("should load all available topics", function (done) {
+      expect(topics).to.respondTo("loadAll");
+
+      topics.load(
+        fixturePath + "topics/topic.json",
+        function (err, topicData) {
+          topics.loadAll(
+            fixturePath + "topics",
+            function (err, allTopics) {
+              expect(allTopics).to.be.an("object");
+              expect(Object.keys(allTopics)).to.have.length(1);
+              expect(allTopics).to.have.property("topic");
+              expect(allTopics.topic).to.deep.equal(topicData);
+              done();
+            }
+          );
+        }
+      );
+    });
+
+    it("should error on an invalid path", function (done) {
+      topics.loadAll(
+        "made up path",
+        function (err, allTopics) {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(allTopics).to.be.undefined;
+
+          return done();
+        }
+      );
+    });
+  });
+
+  describe("get()", function () {
+    it("should return a single topic by id", function (done) {
+      expect(topics).to.respondTo("get");
+
+      topics.load(
+        fixturePath + "topics/topic.json",
+        function (err, expectedData) {
+          var topicData = topics.get("topic");
+          expect(topicData).to.be.an("object");
+          expect(topicData).to.deep.equal(expectedData);
+
+          done();
+        }
+      );
+    });
+
+    it("should handle unknown ids", function () {
+      var topicData = topics.get("made-up-id");
+
+      expect(topicData).to.be.undefined;
+    });
+  });
+
+});
