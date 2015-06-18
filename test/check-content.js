965e8d57ff6ffffddf7904d8273d4dcecf4b4212
Ben Rogers <ben@bdr.org>
2015-06-18 20:35:01 +0100

    Refactored validation tests

---
+++ test/check-content.js
@@ -0,0 +1,51 @@
+var tests  = require("../test-helpers");
+var expect = tests.expect;
+var jjv    = require("jjv");
+var fs     = require("fs");
+
+var books  = require("../lib/books.js");
+var topics = require("../lib/topics.js");
+
+describe("check topics", function () {
+  validate("topic", "content/topics/", topics.load);
+});
+
+describe("check books", function () {
+  validate("book", "content/books/", books.load);
+});
+
+function validate(schemaName, path, loader) {
+  var schemaText = fs.readFileSync("schemas/" + schemaName + ".json", {encoding: "utf8"});
+  expect(schemaText).to.be.a("string");
+  var schema = JSON.parse(schemaText);
+  expect(schema).to.be.an("object");
+
+  var env = jjv();
+  env.addSchema(schemaName, schema);
+
+  var fileList = fs.readdirSync(path);
+
+  fileList.forEach(
+    function (file) {
+      it("validate " + file, function (done) {
+        loader(
+          path + file,
+          function (err, data) {
+            expect(err).to.be.null();
+            expect(data).to.be.an("object");
+
+            var errors = env.validate(schemaName, data);
+
+            if(errors) {
+              throw new Error(JSON.stringify(errors));
+            }
+
+            expect(errors).to.be.null();
+
+            return done();
+          }
+        );
+      });
+    }
+  );
+}
